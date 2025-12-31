14e823d9776be87f4ab337b73f92f74f8d06472b
Daniel Oliveira <dafoliveira13@gmail.com>
2025-12-31 15:47:52 +0000

    feat: Get file path dynamicly from cmd line argument

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import count_words, count_characters, sort_characters_count_desc
 
 def get_book_text(file_path):
@@ -7,10 +9,17 @@ def get_book_text(file_path):
   return file_contents
 
 def main():
-  file_contents = get_book_text("books/frankenstein.txt")
+  book_file_path = None
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  else:
+    book_file_path = sys.argv[1]
+
+  file_contents = get_book_text(book_file_path)
   char_counter_sorted = sort_characters_count_desc(count_characters(file_contents))
   print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
+  print(f"Analyzing book found at {book_file_path}...")
   print("----------- Word Count ----------")
   print(f"Found {count_words(file_contents)} total words")
   print("--------- Character Count -------")
